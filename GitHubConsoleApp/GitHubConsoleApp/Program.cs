935d251e55a72588f51d14496a2034986353d0b0
Nicholas Schembri <Nicholas.Schembri@betway.com>
2016-03-16 13:59:53 +0100

    new test console app created

---
+++ GitHubConsoleApp/GitHubConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Win Technologies">
+//   Copyright Win Technologies
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GitHubConsoleApp
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Fingers crossed.");
+            Console.ReadLine();
+        }
+    }
+}
